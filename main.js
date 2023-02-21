8d4cc0b3bc71ad3c5facfc7212e937cff0aa7109
Shiyaam <shiyaamsunder@users.noreply.github.com>
2023-02-22 04:57:51 +0530

    trying out vite

---
+++ main.js
@@ -0,0 +1,12 @@
+import './style.css'
+
+document.querySelector('#app').innerHTML = `
+  <div>
+    <h1>This is my personal website</h1>
+    <p>
+      Work in Progress
+    </p>
+  </div>
+`
+
+setupCounter(document.querySelector('#counter'))
